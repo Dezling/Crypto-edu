16784671a8768274baa4cce90b1a25b6b7f4cda2
Dezling <odintsov.m.04@mail.ru>
2025-04-20 23:14:49 +0500

    new refactor

--- src/components/GarantSection.vue
+++ src/components/GarantSection.vue
@@ -45,29 +45,29 @@ import { ref } from 'vue'
 
 const trustReasons = ref([
   {
-    title: '7 лет практики',
-    text: 'Реальный опыт работы в криптоарбитраже',
+    title: 'Мой многолетний опыт',
+    text: 'Я многое прошел и нет смысла обманывать людей когда вместе с ними я могу заработать больше',
     icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"/>
           </svg>`
   },
   {
-    title: '500+ учеников',
-    text: 'Успешно завершивших обучение',
+    title: 'Много учеников',
+    text: 'С которыми мы создали очень дружное комьюнити',
     icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"/>
           </svg>`
   },
   {
     title: 'Прозрачность',
-    text: 'Открытая статистика и кейсы учеников',
+    text: 'Открытая статистика и кейсы учеников всегда есть и будут в моем канале',
     icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"/>
           </svg>`
   },
   {
     title: 'Поддержка',
-    text: 'Персональный куратор на всех этапах',
+    text: 'Существует постоянно, это одно из главных моих правил когда я беру учеников',
     icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18.364 5.636l-3.536 3.536m0 5.656l3.536 3.536M9.172 9.172L5.636 5.636m3.536 9.192l-3.536 3.536M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-5 0a4 4 0 11-8 0 4 4 0 018 0z"/>
           </svg>`
